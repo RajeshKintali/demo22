96efeed47fa0aa38bf09a2a90f61f3fe1ba17366
Rajesh <kintalirajesh@gmail.com>
2018-02-26 09:27:46 +0530

    first commit

---
+++ src/main/java/demo11/App.java
@@ -0,0 +1,10 @@
+package demo11;
+
+public class App
+{
+	public String Sample() {
+		
+		return "Sample Program";
+	}
+
+}
---
+++ src/main/java/demo11/app1.java
@@ -0,0 +1,9 @@
+package demo11;
+
+public class app1 {
+
+		public String Sample() {
+			
+			return "Sample Program";
+		}
+}
